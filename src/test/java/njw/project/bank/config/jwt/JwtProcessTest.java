42c65cbdd132a738bde43d418bad24446e97757d
nojiwon1227 <nojiwon1227@gmail.com>
2024-02-02 22:56:20 +0900

    jwt Token 생성 test 코드 작성

---
+++ src/test/java/njw/project/bank/config/jwt/JwtProcessTest.java
@@ -0,0 +1,26 @@
+package njw.project.bank.config.jwt;
+
+import njw.project.bank.config.auth.LoginUser;
+import njw.project.bank.domain.user.User;
+import njw.project.bank.domain.user.UserEnum;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtProcessTest {
+
+    @Test
+    public void create_test() throws Exception {
+
+        User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();
+        LoginUser loginUser = new LoginUser(user);
+        
+        String jwtToken = JwtProcess.create(loginUser);
+        System.out.println("테스트 = " + jwtToken);
+        
+    }
+
+    @Test
+    void verify() {
+    }
+}
