184be739eb4a0d63ddd5c9b2539c62f53accbc00
nojiwon1227 <nojiwon1227@gmail.com>
2024-02-03 23:24:20 +0900

    jwt Token 검증 test 코드 작성

--- src/test/java/njw/project/bank/config/jwt/JwtProcessTest.java
+++ src/test/java/njw/project/bank/config/jwt/JwtProcessTest.java
@@ -5,6 +5,7 @@ import njw.project.bank.domain.user.User;
 import njw.project.bank.domain.user.UserEnum;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JwtProcessTest {
@@ -12,7 +13,7 @@ class JwtProcessTest {
     @Test
     public void create_test() throws Exception {
 
-        User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();
+        User user = User.builder().id(1L).role(UserEnum.ADMIN).build();
         LoginUser loginUser = new LoginUser(user);
         
         String jwtToken = JwtProcess.create(loginUser);
@@ -22,9 +23,14 @@ class JwtProcessTest {
     }
 
     @Test
-    public void verify_tset() throws Exception {
-        String jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5rIiwicm9sZSI6IkNVU1RPTUVSIiwiaWQiOjEsImV4cCI6MTcwNzU3NDI3M30.9pxcdVT-OSSA1OB-xaNUsmjm7ZW2Fz5LW1KUhZrPSlR1tnqTLTStT9DxWi8Pl9z5b76POPLuvVE6aXcL7gdmKQ";
+    public void verify_test() throws Exception {
+        String jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5rIiwicm9sZSI6IkFETUlOIiwiaWQiOjEsImV4cCI6MTcwNzU3NDk4OH0.CvXNhby7V78y_YP1tUlEZ-n6bcRNmsBqD25IfHC9pMJMdBxXP8Q4Vfr0k6oGF9zUt2IV9xwkYNeTAKEYqV14lQ";
+
         LoginUser loginUser = JwtProcess.verify(jwtToken);
         System.out.println("loginUser.getUser().getId() = " + loginUser.getUser().getId());
+        System.out.println("loginUser.getUser().getRole().name() = " + loginUser.getUser().getRole().name());
+
+        assertThat(loginUser.getUser().getId()).isEqualTo(1L);
+        assertThat(loginUser.getUser().getRole()).isEqualTo(UserEnum.ADMIN);
     }
 }
