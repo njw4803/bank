b7c367d229f7027295af26fbae9b933c152e301d
nojiwon1227 <nojiwon1227@gmail.com>
2024-02-06 22:46:57 +0900

    MethodArgumentNotValidException ExceptionHandler 등록

--- src/main/java/njw/project/bank/handler/CustomExceptionHandler.java
+++ src/main/java/njw/project/bank/handler/CustomExceptionHandler.java
@@ -8,8 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
@@ -22,15 +28,27 @@ public class CustomExceptionHandler {
         return new ResponseEntity<>(new ResponseDto<>(-1,e.getMessage(),null), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(CustomForbiddenException.class) // CustomException이 터지면 작동
+    @ExceptionHandler(CustomForbiddenException.class) // CustomException 터지면 작동
     public ResponseEntity<?> forbiddenException(CustomApiException e) {
         log.error(e.getMessage());
         return new ResponseEntity<>(new ResponseDto<>(-1,e.getMessage(),null), HttpStatus.FORBIDDEN);
     }
 
-    @ExceptionHandler(CustomValidationException.class) // CustomValidationException이 터지면 작동
+    @ExceptionHandler(CustomValidationException.class) // CustomValidationException 터지면 작동
     public ResponseEntity<?> validationApiException(CustomValidationException e) {
         log.error(e.getMessage());
         return new ResponseEntity<>(new ResponseDto<>(-1,e.getMessage(),e.getErrorMap()), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) // MethodArgumentNotValidException 터지면 작동
+    public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(e.getMessage());
+        Map<String, String> errors = e.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        fieldError -> Optional.ofNullable(fieldError.getDefaultMessage()).orElse("")
+                ));
+        return new ResponseEntity<>(new ResponseDto<>(-1,e.getMessage(),errors), HttpStatus.BAD_REQUEST);
+    }
+
 }
