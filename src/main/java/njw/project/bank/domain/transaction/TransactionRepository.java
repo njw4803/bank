d5eac0941ffae449f0345f8151e3c7eb4b366a1c
nojiwon1227 <nojiwon1227@gmail.com>
2024-02-06 22:35:10 +0900

    TransactionRepository 수정

--- src/main/java/njw/project/bank/domain/transaction/TransactionRepository.java
+++ src/main/java/njw/project/bank/domain/transaction/TransactionRepository.java
@@ -3,4 +3,5 @@ package njw.project.bank.domain.transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
 }
